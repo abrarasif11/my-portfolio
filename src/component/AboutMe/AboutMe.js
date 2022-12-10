07d4bd08450171c17cfa88a02344f24b270ed165
abrarasif11 <faasif929@gmail.com>
2022-12-10 16:34:12 +0600

    add-favicon

--- src/component/AboutMe/AboutMe.js
+++ src/component/AboutMe/AboutMe.js
@@ -6,6 +6,7 @@ const AboutMe = () => {
             <p className='text-center'>About Myself</p>
              <p>Hello Everyone, I am Fahim Abrar Asif from Khilgaon,Dhaka,Bangladesh.</p>
              <p>I am currently studying </p>
+             <p></p>
         </div>
     );
 };
