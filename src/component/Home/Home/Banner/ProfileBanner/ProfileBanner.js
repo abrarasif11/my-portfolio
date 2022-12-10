5dc24232b2b6b4678d9c2626914bc2e062a4deea
abrarasif11 <faasif929@gmail.com>
2022-12-10 16:31:47 +0600

    resume-button-added

--- src/component/Home/Home/Banner/ProfileBanner/ProfileBanner.js
+++ src/component/Home/Home/Banner/ProfileBanner/ProfileBanner.js
@@ -4,8 +4,8 @@ const ProfileBanner = () => {
     return (
         <div>
             <img className='w-full' src={profile} alt="" />
-            <div>
-                <a href="https://drive.google.com/file/d/16Bx0B_Ac10n-6jCWIJrozbt_e3MfjEPn/view?usp=sharing"><button className='font-me bg-indigo-800'>Download Resume</button></a>
+            <div className=' mt-10 font-montserrat mb-20'>
+                <a href="https://drive.google.com/file/d/16Bx0B_Ac10n-6jCWIJrozbt_e3MfjEPn/view?usp=sharing"><button className=' text-white px-2 py-2 rounded-lg font-me bg-indigo-800'>Download Resume</button></a>
             </div>
         </div>
     );
