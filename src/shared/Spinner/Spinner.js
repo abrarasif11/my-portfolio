668a04782e7810f96963228ee7f8e4a561f96b4c
abrarasif11 <faasif929@gmail.com>
2022-12-11 22:30:45 +0600

    spinner-added

--- src/shared/Spinner/Spinner.js
+++ src/shared/Spinner/Spinner.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 const Spinner = () => {
     return (
-        <div className="w-16 h-16 mt-72 border-4 mx-auto space-x-2">
-            <div className="w-4 h-4 rounded-full animate-pulse bg-indigo-800"></div>
-            <div className="w-4 h-4 rounded-full animate-pulse bg-indigo-800"></div>
-            <div className="w-4 h-4 rounded-full animate-pulse bg-indigo-800"></div>
-        </div>
+        <div className="w-16 h-16 mt-72 border-4 mx-auto border-dashed rounded-full animate-spin border-indigo-800"></div>
     );
 };
 
